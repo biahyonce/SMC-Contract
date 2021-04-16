16598b46ea95d11e906bed75b6b71ad62b0a8354
Bianca <biancacristina-silva@outlook.com>
2021-04-15 21:01:22 -0300

    NatSpec

--- smc-contract/contracts/CommitHandler.sol
+++ smc-contract/contracts/CommitHandler.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.8.0;
 
+/// @title Helper librafy for the SMC contract
+/// @author BiancaCristina
+/// @notice This library can be used to simplify the SMC contract
 library CommitHandler {
     struct Commit {
         address owner;
@@ -8,22 +11,40 @@ library CommitHandler {
         bool[3][4] truthTable;
     }
 
+    /// @notice Generate the first commit structure
+    /// @param commit bytes32 from the commit
+    /// @param truthTable the truth table to be commited
+    /// @return the commit structure generated
     function generate(bytes32 commit, bool[3][4] memory truthTable) public view returns(Commit memory) {
         address owner = msg.sender;
         address previousCommitOwner = msg.sender;
         return Commit(owner, previousCommitOwner, commit, truthTable);
     }
 
+    /// @notice Generate the first commit structure
+    /// @param commit bytes32 from the commit
+    /// @param previousCommitOwner indicates who owns the first commit
+    /// @param truthTable the truth table to be commited
+    /// @return the commit structure generated
     function generate(bytes32 commit, address previousCommitOwner, bool[3][4] memory truthTable) public view returns(Commit memory) {
         address owner = msg.sender;
         return Commit(owner, previousCommitOwner, commit, truthTable);
     }
 
+    /// @notice Verify if the information provided matches the commit
+    /// @param value bytes32 from the commit to be verified
+    /// @param nonce the nonce used to generate the commit
+    /// @param inversion_bits the inversion bits used to generate the commit
+    /// @param encryption_bits the encryption bits used to generate the commit
+    /// @return bool indicating if the information matches the commit
     function verify(bytes32 value, bytes memory nonce, bytes memory inversion_bits, bytes memory encryption_bits) public pure returns(bool) {
         bytes32 generatedValue = sha256(abi.encodePacked(nonce, inversion_bits, encryption_bits));
         return value == generatedValue;
     }
 
+    /// @notice Verify if the commit is the first one
+    /// @param commit the commit to be verified
+    /// @return bool indicating if the commit provided is the first
     function isFirstCommit(Commit memory commit) public pure returns(bool) {
         return commit.owner == commit.previous;
     }
--- smc-contract/contracts/SMC.sol
+++ smc-contract/contracts/SMC.sol
@@ -2,32 +2,59 @@ pragma solidity ^0.8.0;
 
 import {CommitHandler} from "./CommitHandler.sol";
 
+/// @title Contract for Secure Multiparty Computation
+/// @author BiancaCristina
+/// @notice This contract can be use to store truth table commits into Ethereum
 contract SMC {
     using CommitHandler for CommitHandler.Commit;
     mapping (address => CommitHandler.Commit) public commits;
     constructor() {}
 
+    /// @notice Generate the first commit
+    /// @param commit bytes32 from the commit 
+    /// @param truthTable indicates the truth table to be commmited
+    /// @return the commit generated
     function firstCommit(bytes32 commit, bool[3][4] memory truthTable) public returns (CommitHandler.Commit memory) {
         CommitHandler.Commit memory commitGenerated = CommitHandler.generate(commit, truthTable);
         commits[msg.sender] = commitGenerated;
         return commitGenerated;
     }
 
+    /// @notice Generate the second commit
+    /// @param previousCommitOwner indicates who owns the first commit
+    /// @param commit bytes32 from the commit
+    /// @param truthTable indicates the truth table to be commited
+    /// @return the commit generated
     function secondCommit(address previousCommitOwner, bytes32 commit, bool[3][4] memory truthTable) public returns (CommitHandler.Commit memory) {
         CommitHandler.Commit memory commitGenerated = CommitHandler.generate(commit, previousCommitOwner, truthTable);
         commits[msg.sender] = commitGenerated;
         return commitGenerated;
     }
 
+    /// @notice Get the commit using the owner provided
+    /// @param owner the owner's address
+    /// @return the commit related
     function getCommit(address owner) public view returns (CommitHandler.Commit memory) {
         return commits[owner];
     }
     
+    /// @notice Verify if the information provided matches the commit
+    /// @param owner the owner's address
+    /// @param nonce the nonce used to generate the commit
+    /// @param inversion_bits the inversion bits used to generate the commit
+    /// @param encryption_bits the encryption bits used to generate the commit
+    /// @return bool indicating if the information matches the commit
     function verify(address owner, bytes memory nonce, bytes memory inversion_bits, bytes memory encryption_bits) public view returns (bool) {
         bytes32 value = commits[owner].commit;
         return CommitHandler.verify(value, nonce, inversion_bits, encryption_bits);
     }
 
+    /// @notice Computes the final value
+    /// @param owner the owner's address
+    /// @param row the row choosen
+    /// @param first_commit_choices the first commit choices generated during the computation
+    /// @param second_commit_choices the second commit choices generated during the computation
+    /// @return bool indicating the final value
     function getValue(address owner, uint row, bool[2] memory first_commit_choices, bool[2] memory second_commit_choices) public view returns(bool) {
         bool[3][4] memory truthTable = commits[owner].truthTable;
         bool outputEntry = truthTable[row][2];
